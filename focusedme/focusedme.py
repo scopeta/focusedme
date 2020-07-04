9382451046c61e8075a1a520cfe50f0eb1bb5906
scopeta <scopeta@gmail.com>
2020-07-03 22:16:23 -0400

    refactor __main__

--- focusedme/focusedme.py
+++ focusedme/focusedme.py
@@ -8,7 +8,6 @@ as well as allow them to control its progress.
 """
 
 
-import argparse
 import sys
 import time
 from timeit import default_timer
@@ -55,7 +54,7 @@ class View:
 
     @classmethod
     def ring_bell(cls):
-        playsound("Ring01.wav")
+        playsound("focusedme/Ring01.wav")
 
     def show_time(self, remainder, num_round, num_session, type_session):
         """ show timer countdown in terminal. Receives the remainder time
@@ -340,65 +339,3 @@ class Log:
             )
 
         plot(graphic)
-
-
-def main():
-    """ parse cli arguments and start sequence of object calls"""
-
-    print(BANNER, "\n")
-    print(" __ A Pomodoro Timer ___" + "\n\n")
-    parser = argparse.ArgumentParser(
-        description="Welcome to the focusedMe app. Start your Pomodoro timer"
-        " and enjoy the focus! (Stop it with Ctrl+c)",
-        usage="%(prog)s [-f] [-sb] [-lb] [-r]",
-    )
-    parser.add_argument(
-        "-r",
-        "--num_rounds",
-        type=int,
-        metavar="",
-        help="number of rounds, default is 3",
-    )
-    parser.add_argument(
-        "-f",
-        "--focus_time",
-        type=int,
-        metavar="",
-        help="duration in minutes of the focus session, default is 25 mins",
-    )
-    parser.add_argument(
-        "-sb",
-        "--short_break",
-        type=int,
-        metavar="",
-        help="duration in minutes of the short break, default is 5 mins",
-    )
-    parser.add_argument(
-        "-lb",
-        "--long_break",
-        type=int,
-        metavar="",
-        help="duration in minutes of the focus session, default is 25 mins",
-    )
-
-    args = parser.parse_args()
-    # dictionary that store Pomodor initialization parameters
-    len_args = LENGHT_ARGS
-    num_rounds = NUM_ROUNDS
-    if args.focus_time:
-        len_args["focus_time"] = args.focus_time
-    if args.short_break:
-        len_args["short_break"] = args.short_break
-    if args.long_break:
-        len_args["long_break"] = args.long_break
-    if args.num_rounds:
-        num_rounds = args.num_rounds
-
-    # initialize view
-    view = View()
-    # start pomodoro
-    view.run(len_args, num_rounds)
-
-
-if __name__ == "__main__":
-    main()
