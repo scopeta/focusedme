a7a135501d496fe9697ec857a18e02f9417cca11
scopeta <scopeta@gmail.com>
2020-07-04 16:51:43 -0400

    packaging

--- focusedme/__main__.py
+++ focusedme/__main__.py
@@ -62,7 +62,7 @@ class View:
             playsound("Ring01.wav")
         except PlaysoundException:
             # try again with directory path in case
-            #  code is call from module level
+            #  code is call from module level (-m)
             playsound("focusedme\\Ring01.wav")
 
     def show_time(self, remainder, num_round, num_session, type_session):
